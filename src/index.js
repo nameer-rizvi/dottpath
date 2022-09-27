496124f365dbd066c8e5fbc6ad344ad96fe5828d
nameer-rizvi <rizvinameer@gmail.com>
2022-09-27 12:55:04 -0400

    dep update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ const dottpath = {
   diffs: require("./diffs"),
   extract: require("./extract"),
   map: require("./map"),
-  // queryObjects: require("./queryObjects"),
-  // set: require("./set"),
 };
 
 module.exports = dottpath;
